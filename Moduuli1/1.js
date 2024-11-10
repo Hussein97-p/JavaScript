b9031aeaaf6813a9b994049de9b31adb2aa8cc3f
Hussein <alprstigehussein@gmail.com>
2024-11-10 14:17:35 +0200

    Moduuli 1 is done

--- Moduuli1/1.js
+++
@@ -1,8 +0,0 @@
-const age = prompt('Enter your age');
-if (age >= 65) {
-    console.log('You are of retirement age');
-} else if (age>=30) {
-    console.log('You are middle aged.');
-} else if (age>=18) {
-        console.log('You are a young adult');
-}
