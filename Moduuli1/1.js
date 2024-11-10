d4647c6338af821b3b737fd59edd1123a65e62e1
Hussein <alprstigehussein@gmail.com>
2024-11-10 12:59:11 +0200

    Add first JavaScript module (1.js)

---
+++ Moduuli1/1.js
@@ -0,0 +1,8 @@
+const age = prompt('Enter your age');
+if (age >= 65) {
+    console.log('You are of retirement age');
+} else if (age>=30) {
+    console.log('You are middle aged.');
+} else if (age>=18) {
+        console.log('You are a young adult');
+}
